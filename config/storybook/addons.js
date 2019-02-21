17c52dd4042b7fd0bdf0700e5ab292211cf29a46
Ben <ben.k.richter@gmail.com>
2019-02-21 15:22:50 +0100

    Create addons.js

---
+++ config/storybook/addons.js
@@ -0,0 +1,6 @@
+import '@storybook/addons';
+import '@storybook/addon-options/register';
+import 'storybook-addon-react-docgen/register';
+import '@storybook/addon-knobs/register';
+import '@storybook/addon-actions/register';
+import 'storybook-usage/register';
