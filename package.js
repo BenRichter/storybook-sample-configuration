58d2d1aabe326133d94d7c493b0cf72781697f2b
Ben <ben.k.richter@gmail.com>
2019-02-21 15:22:22 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,18 @@
+"scripts": {
+  "test": "TESTING=true node scripts/test.js --env=jsdom",
+  "test:image": "IMG=true node scripts/test.js --env=jsdom",
+  "storybook": "TESTING=true start-storybook -p 6006 -c config/storybook",
+},  
+"devDependencies": {
+  "@storybook/addon-actions": "^4.1.11",
+  "@storybook/addon-info": "^4.1.11",
+  "@storybook/addon-knobs": "^4.1.11",
+  "@storybook/addon-options": "^4.1.11",
+  "@storybook/addon-storyshots": "^4.1.11",
+  "@storybook/addon-storyshots-puppeteer": "^4.1.11",
+  "@storybook/addons": "^4.1.11",
+  "@storybook/react": "^4.1.11",
+  "storybook-addon-react-docgen": "^1.0.4",
+  "storybook-react-router": "^1.0.2",
+  "storybook-usage": "^4.0.7",
+}
